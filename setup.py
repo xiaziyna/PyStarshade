21dbfec85d740e6fb2e34432778d121d3d35c7ba
xiaziyna <xiaziyna@googlemail.com>
2023-04-21 16:11:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pystarshade",
+    version="0.1.0",
+    description="A package for simulating starshade observations",
+    author="Jamila Taaki",
+    author_email="xiaziyna@gmail.com",
+    url="https://github.com/yourusername/pystarshade",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
 
