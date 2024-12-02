c0cb64c3188fdf40f0f0e7eb4865bf4ffad1df7a
xiaziyna <xiaziyna@gmail.com>
2024-12-01 20:22:32 -0500

    restructure for incoherent imaging

--- pystarshade/apodization/apodize.py
+++ pystarshade/apodization/apodize.py
@@ -124,3 +124,21 @@ def grey_pupil_func(Nx, dx = 1, r = 3.):
     full_mask[:Nx//2 + bool_var, :Nx//2 + bool_var] = np.fliplr(np.flipud(grey_mask))
 
     return full_mask
+
+def qu_mask_to_full(grey_mask):
+    """
+    Take the negative quadrant of a starshade mask and return the full mask 
+    Args:
+    grey_mask : Negative quadrant of mask
+   
+    Returns: 
+    full_mask : Full mask made of flipped negative quad
+    """
+    print (np.shape(grey_mask))
+    Nx = Ny = len(grey_mask) - 1
+    full_mask = np.zeros((2*Nx + 1, 2*Ny + 1), dtype=grey_mask.dtype)
+    full_mask[:Nx, Ny+1:] = np.flipud(grey_mask[1:, 1:])
+    full_mask[Nx:, Ny:] = grey_mask
+    full_mask[Nx+1:, :Ny] = np.fliplr(grey_mask[1:, 1:])
+    full_mask[:Nx+1, :Ny+1] = np.fliplr(np.flipud(grey_mask))
+    return full_mask
--- pystarshade/apodization/pupil.py
+++ pystarshade/apodization/pupil.py
@@ -1,51 +1,47 @@
-class Pupil:
-    def __init__(self, filename=None, pupil_type=None, R_p=None, d_s_mas = None, ):
-        """
-        Initializes the Pupil class.
-        Either pass a predefined pupil or generate a new one.
-        Pupil sampling should be an integer fraction of the source resolution in mas
-        unless you're using coherent diffraction. 
-        
-        Parameters:
-        - create_new: bool, if True, creates a new pupil; if False, loads from file.
-        - filename: str, the file path to load the pupil from (used if create_new is False).
-        - size: tuple, the dimensions of the pupil to create if creating a new one.
-        """
+from hcipy import *
+import numpy as np
 
-        self.filename = filename
-        self.pupil_data = None 
-        if self.filename is None: 
-            self.create_pupil(pupil_type)
-        self.pupil_symmetry = 0
-        if pupil_type == 'circ' or 'hex': self.pupil_symmetry = 1
+# From HCIPy tutorial
+def custom_hex(normalized=True):
+    pupil_diameter = 1
+    gap_size = 1e-3 # m
+    num_rings = 2
+    segment_flat_to_flat = (pupil_diameter - (2 * num_rings + 1) * gap_size) / (2 * num_rings + 1)
+    return make_hexagonal_segmented_aperture(num_rings, segment_flat_to_flat, gap_size)
 
-        else:
-            self.d_p_mas = 
-            factor_source_pupil = d_s_mas/d_p_mas 
-            if np.abs(factor_source_pupil%1):
-                from scipy.ndimage import zoom:
-                scale_factor = d_x / d_x_new
+aperture_funcs = {
+    'circ': make_circular_aperture,
+    'ELT': make_elt_aperture,
+    'GMT': make_gmt_aperture,
+    'TMT': make_tmt_aperture,
+    'Hale': make_hale_aperture,
+    'LUVOIR-A': make_luvoir_a_aperture,
+    'LUVOIR-B': make_luvoir_b_aperture,
+    'Magellan': make_magellan_aperture,
+    'VLT': make_vlt_aperture,
+    'HiCAT': make_hicat_aperture,
+    'HabEx': make_habex_aperture,
+    'HST': make_hst_aperture,
+    'JWST': make_jwst_aperture,
+    'Keck': make_keck_aperture,
+    'hex': custom_hex
+}
 
-            self.load_pupil()
+def call_aperture_function(aperture_name, *args, **kwargs):
+    func = aperture_funcs[aperture_name]
+    return func(*args, **kwargs)
 
-    def create_pupil(self):
-        """
-        Creates a new pupil with specified dimensions.
-        """
-        # Placeholder code for creating a new pupil
-        # Example: Initialize self.pupil_data as a 2D array representing the pupil
-        pass
+def make_pupil(N_t, pupil_type='circ'):
+    '''
+    Generate a pupil of a specified type, save it as a compressed file, and return its 2D array representation.
+    Args:
+    N_t : The number of grid points along one dimension for the pupil.
+    pupil_type : The type of pupil aperture to create. Default is 'circ' (circular aperture).
 
-    def interp_pupil(self):
-        """
-        Loads an existing pupil from a file.
-        """
-        # Placeholder code for loading pupil data from a file
-        pass
-
-
-
-# need to create template set of PSF's with 
-
-# First things first, lets reconcile the fresnel methods!
-
+    Returns:
+    The generated pupil is saved to a compressed `.npz` file in the `data/pupils/` directory
+    '''
+    pupil_grid = make_pupil_grid(N_t)
+    pupil = evaluate_supersampled(call_aperture_function(pupil_type, normalized=True), pupil_grid, 8)
+    pupil = np.reshape(pupil, (N_t, N_t))
+    np.savez_compressed('data/pupils/'+pupil_type+'_'+str(int(N_t))+'.npz', pupil=pupil)
