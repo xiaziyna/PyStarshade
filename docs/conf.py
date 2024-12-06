a558a199d664bf1ed5fefd8e53fd405c79ba70c8
xiaziyna <xiaziyna@gmail.com>
2024-12-06 16:08:30 -0500

    make documentation

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ napoleon_google_docstring = False   # Turn off googledoc strings
 napoleon_numpy_docstring = True     # Turn on numpydoc strings
 napoleon_use_ivar = True 	     # For maths symbology
 imgmath_image_format = 'svg'
+highlight_language = 'python'
 
 
 templates_path = ['_templates']
